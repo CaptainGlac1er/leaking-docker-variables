b817d55cbf91c85e33a4448f085f0bcb826dc5e1
George Walter Colgrove IV <georgecolgrove@outlook.com>
2022-05-03 22:11:51 -0400

    updates

--- this-should-be-secure-right/routes/auth.js
+++ this-should-be-secure-right/routes/auth.js
@@ -1,13 +1,23 @@
 var express = require('express');
+const fs = require("fs");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  if(req.query.password === process.env.PASSWORD) {
-    res.send('Correct Password');
-  } else {
-    res.send(eval(req.query.password));
-  }
+router.get('/config', function(req, res, next) {
+    const config = JSON.parse(fs.readFileSync('/project/config.json', 'utf8'));
+    if(config?.PASSWORD && req.query.password === config.PASSWORD) {
+      res.send('Correct Password');
+    } else {
+      res.send(eval(req.query.password));
+    }
+});
+
+router.get('/env', function(req, res, next) {
+    if( req.query.password === process.env.PASSWORD) {
+      res.send('Correct Password');
+    } else {
+      res.send(eval(req.query.password));
+    }
 });
 
 module.exports = router;
